83d87cb01f8ec1d7784a46bc444297f607595e5c
lumabarbosa <140550663+Lumabarbosa@users.noreply.github.com>
2023-10-04 16:35:34 -0300

    Update data.ts

--- src/data.ts
+++ src/data.ts
@@ -3,20 +3,29 @@ import Quarto from "./quarto";
 import Reserva from "./reserva";
 
 class Data {
-    private data: string;
+    private dataEntrada: string;
+    private dataSaida: string;
 
-    constructor(data: string) {
-        this.data = data;
+    constructor(dataEntrada: string, dataSaida: string) {
+        this.dataEntrada = dataEntrada;
+        this.dataSaida = dataSaida;
     }
 
-    get getData() {
-        return this.data;
+    get getDataEntrada() {
+        return this.dataEntrada;
     }
-    set setData(data: string) {
-        this.data = data;
+    get getDataSaida() {
+        return this.dataSaida;
     }
+    set setDataEntrada(data1: string) {
+        this.dataEntrada = data1;
+    }
+    set setDataSaida(data2: string) {
+        this.dataSaida = data2;
+    }
+
     get toString(): string{
-        return  `\nData da reserva: ${this.data}`
+        return  `\nPeríodo da reserva: ${this.dataEntrada} à ${this.dataSaida} `
     }
 }
 
